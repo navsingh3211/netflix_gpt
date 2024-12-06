b94065195e573651e9401397d3260ca57643f034
navsingh3211 <navsingh3211@gmail.com>
2024-12-07 00:03:48 +0530

    first pr

--- netflix-gpt-app/src/App.js
+++ netflix-gpt-app/src/App.js
@@ -1,12 +1,10 @@
 
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello world!</h1>
-      </header>
+      
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      
     </div>
   );
 }
