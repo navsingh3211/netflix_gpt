ff0202543d66b690200748168bce483f71b4b406
navsingh3211 <navsingh3211@gmail.com>
2024-12-10 18:55:26 +0530

    firebase signup and login

--- netflix-gpt-app/README.md
+++ netflix-gpt-app/README.md
@@ -8,13 +8,15 @@
 - Signup form
 - Form validation
 - useRef hook
-- firebase(
+- firebase setup(
     (npm install -g firebase-tools)
     firebase login
     firebase init
     (npm run build)
     firebase deploy
 )
+- Deploying our app to production
+
 
 
 -->npm i -D react-router-dom
--- netflix-gpt-app/src/components/Login.js
+++ netflix-gpt-app/src/components/Login.js
@@ -1,18 +1,66 @@
 import React, { useRef, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+
 import Header from "./Header";
 import { checkValidaData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
-  const [errorMessage,setErrorMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const email = useRef(null);
   const password = useRef(null);
 
-  const handleButtonClick = ()=>{
-    const message = checkValidaData(email.current.value,password.current.value);
-    if(message) setErrorMessage(message);
-  }
+  const handleButtonClick = () => {
+    const message = checkValidaData(
+      email.current.value,
+      password.current.value
+    );
+
+    setErrorMessage(message);
+    if (message) return;
+
+    //Signup + login a new user
+    if (!isSignInForm) {
+      //Signup a new user
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          setIsSignInForm(true);
+          console.log(user, "user");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorMessage, "-", errorCode);
+        });
+    } else {
+      // login a new user
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user, "userloggedIn");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorMessage, "-", errorCode);
+        });
+    }
+  };
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
@@ -25,17 +73,20 @@ const Login = () => {
           alt="BG_LOGO"
         />
       </div>
-      <form onSubmit={(e)=>e.preventDefault()} className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-sm bg-opacity-80">
+      <div
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-sm bg-opacity-80"
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
-        {
-          !isSignInForm && (<input
+        {!isSignInForm && (
+          <input
             type="text"
             placeholder="Full Name"
             className="p-2 my-2 w-full bg-gray-600 rounded-sm"
-          />)
-        }
+          />
+        )}
         <input
           ref={email}
           type="text"
@@ -43,20 +94,24 @@ const Login = () => {
           className="p-2 my-2 w-full bg-gray-600 rounded-sm"
         />
         <input
-            ref={password}
-            type="text"
-            placeholder="Password"
-            className="p-2 my-2 w-full bg-gray-600 rounded-sm"
-          />
+          ref={password}
+          type="text"
+          placeholder="Password"
+          className="p-2 my-2 w-full bg-gray-600 rounded-sm"
+        />
         <p className="text-red-500 py-2 font-bold">{errorMessage}</p>
-        <button className="p-4 my-4 bg-red-700 w-full rounded-sm" onClick={handleButtonClick}>
+        <button
+          className="p-4 my-4 bg-red-700 w-full rounded-sm"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
-        {isSignInForm ? "New to Netflix? Sign Up Now." : "Already registered? Sign In Now."}
-          
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now."
+            : "Already registered? Sign In Now."}
         </p>
-      </form>
+      </div>
     </div>
   );
 };
